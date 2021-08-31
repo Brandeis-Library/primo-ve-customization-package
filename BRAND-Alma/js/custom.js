0dbd137ceae33202b5b78bc6ba801bd3f5dbbf3b
Andrew Parker <amparker@brandeis.edu>
2021-08-31 16:56:36 -0400

    Add Google Tag Manager

--- BRAND-Alma/js/custom.js
+++ BRAND-Alma/js/custom.js
@@ -27,6 +27,13 @@
   })();
   /*---------------libchat code ends here---------------*/
 
+  //Google Tag Manager
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-T489X88');
+
   // LibAnswers System Status widget
 
   (function (s, o, g, a, m) {
