f0e0927c1b894f97ae7662f5d7598cae2d8354ef
Andrew Parker <amparker@brandeis.edu>
2022-09-12 16:18:45 -0400

    Add GA 4 code

--- 01BRAND_INST-BRAND/js/custom.js
+++ 01BRAND_INST-BRAND/js/custom.js
@@ -51,12 +51,28 @@
     });
   })();
 
+  
+
   //Google Tag Manager
   (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
   new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
   })(window,document,'script','dataLayer','GTM-T489X88');
+
+  //Google Analytics 4
+  var googleAnalyticsUrl = document.createElement('script');
+  googleAnalyticsUrl.src = "https://www.googletagmanager.com/gtag/js?id=G-SBKFHHD1QW";
+  googleAnalyticsUrl.type = 'text/javascript';
+  googleAnalyticsUrl.async = true;
+  document.head.appendChild(googleAnalyticsUrl);
+
+  var googleAnalyticsCode = document.createElement('script');
+  googleAnalyticsCode.innerHTML = `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-SBKFHHD1QW');`;
+  document.head.appendChild(googleAnalyticsCode);   
 
   //for continued checking
   setInterval(function () {
